01f850b91312bb128e1461d67258bcaf0da9e137
A. Amrutha Sri <81810889+AmruthaSri852@users.noreply.github.com>
2022-05-19 22:24:49 +0530

    Update 122.cpp

--- SummerChallenge/122.cpp
+++ SummerChallenge/122.cpp
@@ -12,3 +12,12 @@ public:
         return profit;
     }
 };
+
+// Input: Prices = [7,1,5,3,6,4]
+// Output: 7
+
+// Input: Prices = [1,2,3,4,5]
+// Output: 4
+
+// Input: Prices = [7,6,4,3,1]
+// Output: 0
