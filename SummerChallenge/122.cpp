b8a141935cc4513349f6ecfc91826d2029889c16
A. Amrutha Sri <81810889+AmruthaSri852@users.noreply.github.com>
2022-05-19 22:21:02 +0530

    Day 2: Problem-2

---
+++ SummerChallenge/122.cpp
@@ -0,0 +1,14 @@
+//122. Best Time to Buy and Sell Stock II
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0, n = prices.size();
+        
+        for (int i=1; i<n; ++i) {
+            if(prices[i] > prices[i-1])
+                profit += prices[i]-prices[i-1];
+        }
+        return profit;
+    }
+};
