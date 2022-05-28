97ffa6b2b9ccde63d2c506a428c66343d888dba1
A. Amrutha Sri <81810889+AmruthaSri852@users.noreply.github.com>
2022-05-28 22:45:28 +0530

    Day 10: Problem-2

--- SummerChallenge/7.cpp
+++ SummerChallenge/7.cpp
@@ -1,5 +1,14 @@
 // 7. Reverse Integer
 
+// Explaination: for this program, I think many has done this in c programing where division (a/b) is the quotient and modulus (a%b) gives reminder. 
+//     where lets say i be a number 1437, the process goes like j and k, 
+// with 
+// j= 7 while k =143
+//     j=73 while k =14
+//         j=734 while k=1
+//             j=7341 then this the reversed number we need so return j
+    
+
 class Solution {
 public:
     int reverse(int x) {
