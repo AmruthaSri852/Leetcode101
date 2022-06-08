a0bf7ea2f67b5ee4b3ad175a38bcb31a5914f536
A. Amrutha Sri <81810889+AmruthaSri852@users.noreply.github.com>
2022-06-08 19:43:52 +0530

    Day 22: Problem-2

---
+++ SummerChallenge/226.cpp
@@ -0,0 +1,41 @@
+// 226. Invert Binary Tree
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    
+    TreeNode* invertTree(TreeNode* root) {
+        reverse(root);
+        return root;
+    }
+    
+    void reverse(TreeNode* root){
+        if(!root)return;
+        
+        reverse(root->left);       
+        reverse(root->right);       
+        swap(root->left,root->right);  
+        
+    }
+    
+};
+
+// Input: root = [4,2,7,1,3,6,9]
+// Output: [4,7,2,9,6,3,1]
+
+// Input: root = [2,1,3]
+// Output: [2,3,1]
+
+// Input: root = []
+// Output: []
